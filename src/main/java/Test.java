5a8883ba210a9c779eef9c91d3b7bb0b28a2ee16
Aastapchik <154362326+Aastapchik@users.noreply.github.com>
2024-04-04 13:43:40 +0300

    Add files via upload
    add files

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+import org.example.Calc;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Calc.sum(1, 2));
+    }
+}
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(Calc.sum(1, 3));
+        System.out.println(Calc.diff(5, 9));
+        System.out.println(Calc.prod(5, 85));
+        System.out.println(Calc.quotient(5, 0));
+    }
+}
